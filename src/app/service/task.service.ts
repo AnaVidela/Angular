bd29f113c319624df3bbf15c15fa23dc4629d1d3
Ana <analia.videla.84@gmail.com>
2023-01-04 17:28:39 -0300

    para subir al repositorio de Git

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler} from '@angular/common/http';
-import { from, Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
+
+const httpOptions ={
+  headers: new HttpHeaders ({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +23,16 @@ export class TaskService {
   
   getTasks() : Observable<Task []>{
     return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  deleteTask(task:Task): Observable<Task>{
+    const url= `s{this.apiUrl}/s{task.id}` ;
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder (task:Task): Observable<Task> {
+    const url =	`s{this.apiUrl}/s{task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
   }
 
 }
